ccd9504533194abec6ef8e6a0df6e32159a868db
Christopher Rogers <chrisrogerstezcat@gmail.com>
2016-12-15 13:13:52 -0600

    student checkin updates
    checkin button has an array of checked students, no sending yet

--- src/pages/student-checkin/student-checkin.ts
+++ src/pages/student-checkin/student-checkin.ts
@@ -12,6 +12,7 @@ import { StudentDetailsPage } from '../student-details/student-details';
     }
     .studentID{
       color: grey;
+      font-size: 0.75em;
     }
   `
   ]
@@ -25,42 +26,42 @@ export class StudentCheckinPage {
 	//this.checkedStudents = [];
     this.students = [];
     this.students.push({
-	  id: 1,
+	  id: 111,
       title: 'Matthew',
       note: 'checked in at 8:15am',
       icon: 'man',
       selected: false
     });
     this.students.push({
-	  id: 2,
+	  id: 112,
       title: 'Mark',
       note: 'checked in at 8:05am',
       icon: 'man',
       selected: false,
     });
     this.students.push({
-	  id: 3,
+	  id: 213,
       title: 'Sarah',
       note: 'checked in at 8:05am',
       icon: 'woman',
       selected: false
     });
     this.students.push({
-	  id: 4,
+	  id: 324,
       title: 'Laurel',
       note: 'checked in at 7:55am',
       icon: 'woman',
       selected: false
     });
     this.students.push({
-	  id: 5,
+	  id: 543,
       title: 'Luke',
       note: 'checked in at 8:10am',
       icon: 'man',
       selected: false
     });
     this.students.push({
-	  id: 6,
+	  id: 612,
       title: 'Katie',
       note: 'checked in at 8:00am',
       icon: 'woman',
@@ -74,7 +75,10 @@ export class StudentCheckinPage {
   
   studentTapped(event, student) {
 	  student.selected = !student.selected;
-    
-    
+  }
+  
+  checkIn(event){
+    let checkedStudents = this.students.filter((student) => {return student.selected});
+    //this.navCtrl.push(this.studentCheckinPage, checkedStudents);
   }
 }
