e98b6a85da4ef17131e05f33683a9c85c3209171
Nate Whitesell <njwhites@uark.edu>
2017-01-10 12:05:10 -0600

    horizontal padding added

--- src/pages/student-details/student-details.ts
+++ src/pages/student-details/student-details.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from "@angular/core";
 import {NavController, NavParams} from "ionic-angular";
+import {CheckinProvider} from '../../providers/checkin-provider';
 
 @Component({
   selector: 'page-student-details',
@@ -8,10 +9,8 @@ import {NavController, NavParams} from "ionic-angular";
 export class StudentDetailsPage {
   @Input() selectedStudent: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public checkinService: CheckinProvider) {
     // If we navigated to this page, we will have a student available as a nav param
     this.selectedStudent = navParams.get('student');
-    
-    
   }
 }
