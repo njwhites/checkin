a87c53450a0c0aca7a7a7729eb7842d36b2e0fe3
Christopher Rogers <chrisrogerstezcat@gmail.com>
2017-01-27 13:43:29 -0600

    Commented classroom modal

--- src/pages/admin-classroom-modal/admin-classroom-modal.ts
+++ src/pages/admin-classroom-modal/admin-classroom-modal.ts
@@ -38,37 +38,41 @@ export class AdminClassroomModalPage {
     console.log("tried to delete room " + this.classroom._id);
   }
 
+  //remove a student from a class room
+  //takes as input the student id for the student to be removed from the classroom
   removeStudent(SID){
+    //ask the classroom provider to remove the student by passing it the classroom to remove from and the student id to remove
     this.classroomService.removeStudentFromClass(this.classroom, SID);
 
+    //now that the student is removed from the class room we should add them to the "unallocated" classroom which is id'd by -1
+    //then pass the classroom service the "unallocated" classroom and student id to add together
     this.classroomService.getClassRoomByID("-1").then((classroom:ClassRoomModel)=>{
       this.classroomService.addStudentToClass(classroom, SID);
     });
   }
 
+  //Chris 1/27/17 this method of adding students involves opening an alert and selecting students from there
+  //might be replaced with another modal or page that is more similar to the therapist add page
   addStudentAlert(){
     console.log("tried to add student");
 
+    //create the alert to display optional students
     let alert = this.alertController.create({
       title: 'Select Students To Add'
     });
+
+    //build an array of students that don't already belong to the classroom we are in
     Array.from(this.studentService.data.values()).filter((value)=>{
       return this.classroom.students.indexOf(value._id)<0;
+
+      //for each student add a checkbox to the alert
     }).forEach((value, index, array)=>{
       //pick one of the below input or button
       alert.addInput({
-            type: 'checkbox',
-            label: value.fName + ' ' + value.lName,
-            value: ''+value._id
-          });
-
-
-      // alert.addButton({
-      //   text: value.roomNumber,
-      //   handler: ()=>{
-      //     this.onSelectClassroom(value.roomNumber);
-      //   }
-      // })
+        type: 'checkbox',
+        label: value.fName + ' ' + value.lName,
+        value: ''+value._id
+      });
     });
     alert.addButton({
       text: "Cancel",
@@ -77,6 +81,7 @@ export class AdminClassroomModalPage {
 
       }
     })
+
     alert.addButton({
       text: "Okay",
       handler: (data)=>{
@@ -88,14 +93,20 @@ export class AdminClassroomModalPage {
 
   }
 
+  //function that takes in a student id and requests that the classroom provider add it to the class
   addStudent(SID){
+    //for tracking and maybe user feedback purposes
+    //save the previous room for future display
     let previousRoom: ClassRoomModel;
+
+    //first we want to get the student from the previous room and remove them
     this.classroomService.getAllClassRooms().then((classrooms:Array<ClassRoomModel>)=>{
       previousRoom = classrooms.find((value)=>{return value.students.indexOf(SID) >= 0});
 
       this.classroomService.removeStudentFromClass(previousRoom, SID);
     });
 
+    //now we add the student to the current room
     this.classroomService.addStudentToClass(this.classroom, SID);
 
   }
