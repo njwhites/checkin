7b67a458ac257e639a385560b9a01b31ded6b1f4
jaswinne <jaswinne@uark.edu>
2017-01-27 11:19:12 -0600

    kitchen now live updates and spits numbers to the screen

--- src/pages/kitchen/kitchen.ts
+++ src/pages/kitchen/kitchen.ts
@@ -14,21 +14,57 @@ import { CheckinProvider } from '../../providers/checkin-provider'
 })
 export class KitchenPage {
 
+  allClassrooms: Array<ClassRoomModel> = [];
+  roomMap : any;
+  refresh: any;
+
   constructor(public navCtrl: NavController, public classroomService: ClassRoomProvider, public studentService: StudentProvider, public checkinService: CheckinProvider) {
   	classroomService.getAllClassRooms()
   		.then((result: Array<ClassRoomModel>) => {
-  			result.forEach((classroom: ClassRoomModel) =>{
-  				console.log(classroom.roomNumber);
-  				let hereStudents = classroom.students.filter(id => {					
-  					return studentService.data.get(id).location !== checkinService.CHECKED_OUT;
-  				});
-  				let diet = hereStudents.filter(id => {
-  					return studentService.data.get(id).dietNeed !== undefined && studentService.data.get(id).dietNeed === true;
-  				})
-  				console.log(hereStudents);
-  				console.log(`Normies: ${hereStudents.length - diet.length} Others: ${diet.length}`);
-  			});
+        this.allClassrooms = result;
+        this.setRoomMap();
   		});
   }
 
+  setRoomMap(){
+    let map = [];
+    this.allClassrooms.forEach((classroom: ClassRoomModel) =>{
+      if(classroom.roomNumber.toLowerCase() !== 'unallocated'){
+        console.log(classroom.roomNumber);
+          if(classroom.roomNumber === "101"){
+             console.log(this.studentService.data);
+           }
+        let hereStudents = classroom.students.filter(id => {      
+           if(classroom.roomNumber === "101"){
+             console.log(this.studentService.data.get(id).location);
+           }
+          return this.studentService.data.get(id).location !== this.checkinService.CHECKED_OUT;
+        });
+        let diet = hereStudents.filter(id => {
+          return this.studentService.data.get(id).dietNeed !== undefined && this.studentService.data.get(id).dietNeed === true;
+        })
+        console.log(hereStudents);
+        console.log(`Normies: ${hereStudents.length - diet.length} Others: ${diet.length}`);
+        map.push({key: classroom.roomNumber, val: [hereStudents.length, diet. length]});  
+      }
+    });
+    console.log(map);
+    this.roomMap = map;
+  }
+
+  ionViewDidEnter(){
+    if(this.refresh){
+      clearInterval(this.refresh);
+    }
+    this.refresh = setInterval(() => {
+      console.log("Refreshing");
+      this.studentService.getStudents().then(() => {
+        this.setRoomMap();
+      })
+    }, 15000)
+  }
+
+  ionViewDidLeave(){
+    clearInterval(this.refresh);
+  }
 }
