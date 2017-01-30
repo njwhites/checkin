b1d5c2c21c71111b3ad0415cc17b0d4ad5410ca6
N Whitesell <njwhites2012@gmail.com>
2017-01-30 13:26:16 -0600

    students form therapist's favorites list are removable

--- src/pages/therapist/therapist.ts
+++ src/pages/therapist/therapist.ts
@@ -61,4 +61,24 @@ export class TherapistPage {
     });
   }
 
+  //remove a student from therapist faorite ids
+  //takes as input the student id for the student to be removed from the list
+  removeStudent(SID: String){
+    //ask the user provider to remove the student from the favorited list
+    this.therapistStudents.splice(this.therapistStudents.indexOf(SID.toString()), 1);
+
+    this.userService.removeTherapistFavoriteID(String(this.id), SID).then((success) => {
+      if(success){
+        //now that the student is removed, update the list
+        this.userService.getTherapistFavoriteIDs(this.id.toString()).then((result:any) => {
+          this.therapistStudents = result;
+        }).catch((err) => {
+          console.log(err);
+        });
+      }
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
 }
