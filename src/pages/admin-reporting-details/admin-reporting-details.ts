5efc852ce9513649df4a87c9a1ca3665f6d96e9d
Christopher Rogers <chrisrogerstezcat@gmail.com>
2017-02-17 16:23:53 -0600

    admin reporting details updates
    Updated admin reporting details to have editable fields that update the
    derived fields.
    
    this also modifies the data object in the admin reporting page, just
    needs to refresh the html/reducer totals

--- src/pages/admin-reporting-details/admin-reporting-details.ts
+++ src/pages/admin-reporting-details/admin-reporting-details.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Validators, FormBuilder, FormControl } from '@angular/forms';
 
 import { CheckinProvider } from '../../providers/checkin-provider'
 import { StudentProvider } from '../../providers/student-provider'
@@ -26,8 +27,11 @@ export class AdminReportingDetailsPage {
   reducer: any;
   totals: BillingDay;
 
+  studentDataForm: any;
+
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
+              public formBuilder: FormBuilder,
               public checkinService: CheckinProvider,
               public studentService: StudentProvider) {
     this.student = navParams.get("student");
@@ -43,9 +47,120 @@ export class AdminReportingDetailsPage {
     this.reducer = navParams.get("reducer");
     this.totals = this.student.student_days.reduce(this.reducer);
     console.log(this.student);
+
+    //construct the form for all of the days data here
+    this.studentDataForm = this.formBuilder.group({
+      MTimeIn: [,Validators.required],
+      MTimeOut: [, Validators.required],
+      MNapHours: [, Validators.required],
+      MSP: [, Validators.required],
+      MPT: [, Validators.required],
+      MOT: [, Validators.required],
+      TTimeIn: [,Validators.required],
+      TTimeOut: [, Validators.required],
+      TNapHours: [, Validators.required],
+      TSP: [, Validators.required],
+      TPT: [, Validators.required],
+      TOT: [, Validators.required],
+      WTimeIn: [,Validators.required],
+      WTimeOut: [, Validators.required],
+      WNapHours: [, Validators.required],
+      WSP: [, Validators.required],
+      WPT: [, Validators.required],
+      WOT: [, Validators.required],
+      THTimeIn: [,Validators.required],
+      THTimeOut: [, Validators.required],
+      THNapHours: [, Validators.required],
+      THSP: [, Validators.required],
+      THPT: [, Validators.required],
+      THOT: [, Validators.required],
+      FTimeIn: [,Validators.required],
+      FTimeOut: [, Validators.required],
+      FNapHours: [, Validators.required],
+      FSP: [, Validators.required],
+      FPT: [, Validators.required],
+      FOT: [, Validators.required]
+    });
   }
 
   ionViewDidLoad() {
+  }
+
+//method to be called whenever the student data form is changed
+  formChanged(event){
+
+    //temporary variable to store the two inputs from the split id
+    let temp = event.target.id.split(',');
+
+    //first number in id = the day that was modified
+    let day = temp[0];
+    //second number in id = the field that was modified
+    let field = temp[1];
+
+    //switch on the field so that we only compute the field we need
+    //all cases are set the student field equal to the changed value, casting as a number incase we need that to do .toFixed or anything similar
+    switch(field){
+      case("0"):
+        //TODO extra logic to transform user input into a number the data model likes, similar for end_time
+        this.student.student_days[day].start_time = <number>(event.target.value);
+        break;
+      case("1"):
+        this.student.student_days[day].end_time = <number>(event.target.value);
+        break;
+      case("2"):
+        this.student.student_days[day].nap_hours = <number>(event.target.value);
+        break;
+      case("3"):
+        this.student.student_days[day].SP_therapy_hours = <number>(event.target.value);
+        break;
+      case("4"):
+        this.student.student_days[day].PT_therapy_hours = <number>(event.target.value);
+        break;
+      case("5"):
+        this.student.student_days[day].OT_therapy_hours = <number>(event.target.value);
+        break;
+      default:
+        break;
+    }
+
+    //recompte the derived fields since one of the fields they were derived from possibly changed
+
+    //gross hours and the way end and begin time are stored has some design decisions to be made
+    //this.student.student_days[day].gross_hours = this.student.student_days[day].end_time - this.student.student_days[day].start_time;
+
+    this.student.student_days[day].net_hours = this.student.student_days[day].gross_hours
+                                               - this.student.student_days[day].nap_hours
+                                               - this.student.student_days[day].SP_therapy_hours
+                                               - this.student.student_days[day].PT_therapy_hours
+                                               - this.student.student_days[day].OT_therapy_hours;
+
+    this.student.student_days[day].billable_hours = Math.min(5, Math.trunc(this.student.student_days[day].net_hours));
+
+    //reevaluate the conditions for warnings
+    if(this.student.student_days[day].gross_hours < 7){
+      this.student.student_days[day].attendanceWarning = true;
+    } else {
+      this.student.student_days[day].attendanceWarning = false;
+    }
+
+    if(this.student.student_days[day].billable_hours < 5){
+      this.student.student_days[day].billingWarning = true;
+    } else {
+      this.student.student_days[day].billingWarning = false;
+    }
+
+
+    if( (this.student.student_days[day].SP_therapy_hours
+        + this.student.student_days[day].PT_therapy_hours
+        + this.student.student_days[day].OT_therapy_hours) > 1){
+      this.student.student_days[day].therapyWarning = true;
+    } else {
+      this.student.student_days[day].therapyWarning = false;
+    }
+
+    //recompute the totals since things have changed
+    this.totals = this.student.student_days.reduce(this.reducer);
+
   }
 
 }
