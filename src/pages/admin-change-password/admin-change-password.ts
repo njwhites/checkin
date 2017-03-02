ed33ba0e6c4dd9243ddfbc707246cc375f53220e
jaswinne <jaswinne@uark.edu>
2017-03-02 13:11:22 -0600

    can change password

--- src/pages/admin-change-password/admin-change-password.ts
+++ src/pages/admin-change-password/admin-change-password.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { AuthProvider } from '../../providers/auth-provider'
 
 @Component({
   selector: 'page-admin-change-password',
   templateUrl: 'admin-change-password.html'
 })
 export class AdminChangePasswordPage {
-  id: number;
+  id: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.id = navParams.get("userID");
+  constructor(public navCtrl: NavController, public navParams: NavParams, public authService: AuthProvider) {
+    this.id = navParams.get("userID") + "";
   }
 
   ionViewDidLoad() {
   }
 
   updatePassword(password, newPassword, reNewPassword){
+  	this.authService.checkPassword(this.id, password).then((success) => {
+  		if(success){
+  			this.authService.setPassword(this.id, newPassword);
+  		}
+  	})
     console.log("id: " + this.id);
     console.log("password change form: " + password + " " + newPassword + " " + reNewPassword)
   }
