c7a6a6bc69819eca2b64b082a4454d8fb23cd7f2
Christopher Rogers <chrisrogerstezcat@gmail.com>
2017-03-28 12:04:52 -0500

    silence logging provider
    logging provider was spamming console with conflicts so we are going to
    turn it off till we decide we need it

--- src/providers/logging-provider.ts
+++ src/providers/logging-provider.ts
@@ -8,33 +8,33 @@ export class LoggingProvider {
 
   constructor() {
 
-    this.db = new PouchDB('http://104.197.130.97:5984/logging', {
-      ajax: {
-
-      },
-      auth: {
-        username: 'chris',
-        password: 'couchdbadmin5'
-      }
-    });
+    // this.db = new PouchDB('http://104.197.130.97:5984/logging', {
+    //   ajax: {
+    //
+    //   },
+    //   auth: {
+    //     username: 'chris',
+    //     password: 'couchdbadmin5'
+    //   }
+    // });
   }
 
   //writes to the db a log string with a key as timestamp
   writeLog(logString: string): boolean{
-    let now = new Date();
-    let dateString = now.getFullYear()+"-"+(now.getMonth() + 1)+"-"+(now.getDate());
-
-    this.db.upsert(dateString, (doc)=>{
-      doc[Date.now()] = logString;
-      return doc;
-    }).then(()=>{
-      //success!
-      return true;
-    }).catch((err)=>{
-      console.log(err);
-      //something went wrong
-      return false;
-    })
+    // let now = new Date();
+    // let dateString = now.getFullYear()+"-"+(now.getMonth() + 1)+"-"+(now.getDate());
+    //
+    // this.db.upsert(dateString, (doc)=>{
+    //   doc[Date.now()] = logString;
+    //   return doc;
+    // }).then(()=>{
+    //   //success!
+    //   return true;
+    // }).catch((err)=>{
+    //   console.log(err);
+    //   //something went wrong
+    //   return false;
+    // })
 
     return false;
 
@@ -47,12 +47,12 @@ export class LoggingProvider {
 
   //return the logging data for a specific day
   loggingDataByDay(inputDay:Date){
-    let searchDate = inputDay.getFullYear()+"-"+(inputDay.getMonth() + 1)+"-"+(inputDay.getDate());
-    this.db.get(searchDate).then((doc)=>{
-      return doc;
-    }).catch((err)=>{
-      console.log(err);
-      return {};
-    })
+    // let searchDate = inputDay.getFullYear()+"-"+(inputDay.getMonth() + 1)+"-"+(inputDay.getDate());
+    // this.db.get(searchDate).then((doc)=>{
+    //   return doc;
+    // }).catch((err)=>{
+    //   console.log(err);
+    //   return {};
+    // })
   }
 }
