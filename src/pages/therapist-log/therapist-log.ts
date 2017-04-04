9ff888003314575a924158a1b8082bb4e392cec8
Nate Whitesell <njwhites@uark.edu>
2017-04-04 13:13:04 -0500

    Therapist log started up

--- src/pages/therapist-log/therapist-log.ts
+++ src/pages/therapist-log/therapist-log.ts
@@ -1,22 +1,47 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {StudentProvider} from '../../providers/student-provider';
+import {CheckinProvider} from "../../providers/checkin-provider";
+import {UserProvider} from "../../providers/user-provider";
+import { NavController, NavParams } from 'ionic-angular';
+import {TransactionTherapy} from "../../models/db-models"
+import {StudentModel} from "../../models/db-models"
 
-/*
-  Generated class for the TherapistLog page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+
 @Component({
   selector: 'page-therapist-log',
   templateUrl: 'therapist-log.html'
 })
 export class TherapistLogPage {
+  id: Number;
+  transactions: Array<any> = new Array<any>();
+  fav_ids: any;
+  studentList: any;
+  location: string;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public checkinService: CheckinProvider,
+              public studentService: StudentProvider,
+              public userService: UserProvider) {
+    this.id = navParams.data;
+    this.fav_ids = this.userService.data.get(String(this.id)).therapy_fav_ids;
+    this.studentService.getStudents().then(()=> {
+      this.studentList = this.studentService.data;
+    }).catch((err) => {
+      console.log(err);
+    });
+    console.log(this.studentList)
+    for(let student in this.studentList){
+      console.log("Student: " + student)
+    }
+  }
 
   ionViewDidLoad() {
-    console.log('Hello TherapistLogPage Page');
+  }
+
+  getTherapistName(_id: String) {
+    return this.userService.data.get(String(_id)).fName + " " + this.userService.data.get(String(_id)).lName;
   }
 
 }
