f7ae0dd9e55bd18a17996920cd250a38e6279b30
Nate Whitesell <njwhites@uark.edu>
2017-04-06 11:16:12 -0500

    Driver page complete

--- src/pages/driver/driver.ts
+++ src/pages/driver/driver.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {NavController, NavParams} from "ionic-angular";
+import {NavController, NavParams, AlertController } from "ionic-angular";
 import {StudentProvider} from '../../providers/student-provider';
 import {CheckinProvider} from '../../providers/checkin-provider';
 import {UserProvider} from '../../providers/user-provider';
@@ -19,15 +19,27 @@ export class DriverPage {
               public navCtrl: NavController,
               public navParams: NavParams,
               public checkinService: CheckinProvider,
-              public userService: UserProvider) {
+              public userService: UserProvider,
+              public alertCtrl: AlertController) {
       this.id = navParams.data;
       this.studentList = this.studentService.data;
       this.user = this.userService.data.get(String(this.id));
       this.driverStudents = this.user.visible_students;
   }
 
-  ionViewDidLoad() {
+  ionViewDidLoad() {}
+
+  seeNotes(thisStudent){
+    if((this.studentList.get(thisStudent).note || '').length !== 0){
+     let alert = this.alertCtrl.create({
+       title: this.studentList.get(thisStudent).fName + ' ' + this.studentList.get(thisStudent).lName + "'s Notes",
+       subTitle: this.studentList.get(thisStudent).note,
+       buttons: ['Got It!']
+     });
+     alert.present();
+   }
   }
+
   getDriverName(_id: String) {
     return this.user.fName + " " + this.user.lName;
   }
